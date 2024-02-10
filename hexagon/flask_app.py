414eee4dd5b3b68ad211e3dac30e91ff354e7516
hb22-eng <harobryan22@gmail.com>
2024-02-10 14:44:03 -0600

    flask app starter

---
+++ hexagon/flask_app.py
@@ -0,0 +1,19 @@
+import pandas
+import requests
+from flask import Flask
+from amadeus.api_call import Api_call
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return 'Welcome!'
+
+@app.route('/flight_searcher')
+def flight_searcher():
+    Api_call().flight_offers(departure_date="2024-11-02",access_token=Api_call().get_access_token())
+    return 'Got flights'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
